6089d6491220e30acd18529e5369ba45cd28f027
ortaoriarp <ilhamiyilmaz@yahoo.com>
2017-05-15 16:14:24 +0300

    Add files via upload

---
+++ js/karayel.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+    //$('.bxslider').bxSlider({
+    //    controls: false,
+    //    pager: false,
+    //    auto: true,
+    //    pause: 3000,
+    //    mode: "fade"
+    //});
+
+    //Eski Owl Carousel Trigger
+    //$("#owl-example").owlCarousel({
+    //    autoPlay: 3000
+    //});
+
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ["Önceki", "Sonraki"],
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: false,
+        loop: true,
+        animateOut: "fadeOut"
+    });
+
+    $(".owl-urun").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ["<i class=\"fa fa-chevron-left\"></i>", "<i class=\"fa fa-chevron-right\"></i>"],
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        loop: true,
+        animateOut: "fadeOut",
+        thumbs: true,
+        thumbsPrerendered: true
+    });
+});
+
+if ($(".fancybox").length) {
+    $(document).ready(function () {
+        $('.fancybox').fancybox();
+    });
+}
+
+if ($(".fancybox2").length) {
+    $(document).ready(function () {
+        $(".fancybox2").fancybox({
+            arrows: false
+        });
+    });
+}
+
+if ($(".fancybox3").length) {
+    $(document).ready(function () {
+        $(".fancybox3").fancybox({
+            arrows: false
+        });
+    });
+}
+
+$('.animasyonlu').hover(
+       function () { $(this).addClass('animated pulse') },
+       function () { $(this).removeClass('animated pulse') }
+)
+
+function karayelNavFunc() {
+    if ((navigator.platform.indexOf("iPhone") != -1)
+        || (navigator.platform.indexOf("iPod") != -1))
+        window.open("maps://maps.google.com/maps?q=36.854798,30.756845");
+    else
+        window.open("http://maps.google.com/maps?q=36.854798,30.756845");
+}
+
+new WOW().init();
---
+++ js/tema.js
@@ -0,0 +1,173 @@
+$(document).ready(function () {
+    $('.pie_progress').asPieProgress({
+        namespace: 'pie_progress'
+    });
+
+    $('#modalArcadia').on('shown.bs.modal', function () {
+        $('.pie_progress_Arcadia').asPieProgress('start');
+    });
+    $('#modalArcadia').on('hidden.bs.modal', function () {
+        $('.pie_progress_Arcadia').asPieProgress('reset');
+    });
+
+    $('#modalArcana').on('shown.bs.modal', function () {
+        $('.pie_progress_Arcana').asPieProgress('start');
+    });
+    $('#modalArcana').on('hidden.bs.modal', function () {
+        $('.pie_progress_Arcana').asPieProgress('reset');
+    });
+
+    $('#modalArsonist').on('shown.bs.modal', function () {
+        $('.pie_progress_Arsonist').asPieProgress('start');
+    });
+    $('#modalArsonist').on('hidden.bs.modal', function () {
+        $('.pie_progress_Arsonist').asPieProgress('reset');
+    });
+
+    $('#modalB-Best').on('shown.bs.modal', function () {
+        $('.pie_progress_B-Best').asPieProgress('start');
+    });
+    $('#modalB-Best').on('hidden.bs.modal', function () {
+        $('.pie_progress_B-Best').asPieProgress('reset');
+    });
+
+    $('#modalBannermaster').on('shown.bs.modal', function () {
+        $('.pie_progress_Bannermaster').asPieProgress('start');
+    });
+    $('#modalBannermaster').on('hidden.bs.modal', function () {
+        $('.pie_progress_Bannermaster').asPieProgress('reset');
+    });
+
+    $('#modalBatman').on('shown.bs.modal', function () {
+        $('.pie_progress_Batman').asPieProgress('start');
+    });
+    $('#modalBatman').on('hidden.bs.modal', function () {
+        $('.pie_progress_Batman').asPieProgress('reset');
+    });
+
+    $('#modalBattleborn').on('shown.bs.modal', function () {
+        $('.pie_progress_Battleborn').asPieProgress('start');
+    });
+    $('#modalBattleborn').on('hidden.bs.modal', function () {
+        $('.pie_progress_Battleborn').asPieProgress('reset');
+    });
+
+    $('#modalBigBird').on('shown.bs.modal', function () {
+        $('.pie_progress_BigBird').asPieProgress('start');
+    });
+    $('#modalBigBird').on('hidden.bs.modal', function () {
+        $('.pie_progress_BigBird').asPieProgress('reset');
+    });
+
+    $('#modalBigJ').on('shown.bs.modal', function () {
+        $('.pie_progress_BigJ').asPieProgress('start');
+    });
+    $('#modalBigJ').on('hidden.bs.modal', function () {
+        $('.pie_progress_BigJ').asPieProgress('reset');
+    });
+
+    $('#modalBlindwolf').on('shown.bs.modal', function () {
+        $('.pie_progress_Blindwolf').asPieProgress('start');
+    });
+    $('#modalBlindwolf').on('hidden.bs.modal', function () {
+        $('.pie_progress_Blindwolf').asPieProgress('reset');
+    });
+    
+    $('#modalBlueRose').on('shown.bs.modal', function () {
+        $('.pie_progress_BlueRose').asPieProgress('start');
+    });
+    $('#modalBlueRose').on('hidden.bs.modal', function () {
+        $('.pie_progress_BlueRose').asPieProgress('reset');
+    });
+
+    $('#modalBullbill').on('shown.bs.modal', function () {
+        $('.pie_progress_Bullbill').asPieProgress('start');
+    });
+    $('#modalBullbill').on('hidden.bs.modal', function () {
+        $('.pie_progress_Bullbill').asPieProgress('reset');
+    });
+
+    $('#modalConsumer').on('shown.bs.modal', function () {
+        $('.pie_progress_Consumer').asPieProgress('start');
+    });
+    $('#modalConsumer').on('hidden.bs.modal', function () {
+        $('.pie_progress_Consumer').asPieProgress('reset');
+    });
+
+    $('#modalAida').on('shown.bs.modal', function () {
+        $('.pie_progress_Aida').asPieProgress('start');
+    });
+    $('#modalAida').on('hidden.bs.modal', function () {
+        $('.pie_progress_Aida').asPieProgress('reset');
+    });
+
+    $('#modalCoward').on('shown.bs.modal', function () {
+        $('.pie_progress_Coward').asPieProgress('start');
+    });
+    $('#modalCoward').on('hidden.bs.modal', function () {
+        $('.pie_progress_Coward').asPieProgress('reset');
+    });
+    
+    $('#modalCrimson').on('shown.bs.modal', function () {
+        $('.pie_progress_Crimson').asPieProgress('start');
+    });
+    $('#modalCrimson').on('hidden.bs.modal', function () {
+        $('.pie_progress_Crimson').asPieProgress('reset');
+    });
+    
+    $('#modalCrowman').on('shown.bs.modal', function () {
+        $('.pie_progress_Crowman').asPieProgress('start');
+    });
+    $('#modalCrowman').on('hidden.bs.modal', function () {
+        $('.pie_progress_Crowman').asPieProgress('reset');
+    });
+
+    $('#modalCruel').on('shown.bs.modal', function () {
+        $('.pie_progress_Cruel').asPieProgress('start');
+    });
+    $('#modalCruel').on('hidden.bs.modal', function () {
+        $('.pie_progress_Cruel').asPieProgress('reset');
+    });
+
+    $('#modalCruella').on('shown.bs.modal', function () {
+        $('.pie_progress_Cruella').asPieProgress('start');
+    });
+    $('#modalCruella').on('hidden.bs.modal', function () {
+        $('.pie_progress_Cruella').asPieProgress('reset');
+    });
+
+    $('#modalDestructa-X').on('shown.bs.modal', function () {
+        $('.pie_progress_Destructa-X').asPieProgress('start');
+    });
+    $('#modalDestructa-X').on('hidden.bs.modal', function () {
+        $('.pie_progress_Destructa-X').asPieProgress('reset');
+    });
+
+    $('#modalEagleQueen').on('shown.bs.modal', function () {
+        $('.pie_progress_EagleQueen').asPieProgress('start');
+    });
+    $('#modalEagleQueen').on('hidden.bs.modal', function () {
+        $('.pie_progress_EagleQueen').asPieProgress('reset');
+    });
+
+    $('#modalFarCry').on('shown.bs.modal', function () {
+        $('.pie_progress_FarCry').asPieProgress('start');
+    });
+    $('#modalFarCry').on('hidden.bs.modal', function () {
+        $('.pie_progress_FarCry').asPieProgress('reset');
+    });
+
+    $('#modalCrimsonPoison').on('shown.bs.modal', function () {
+        $('.pie_progress_CrimsonPoison').asPieProgress('start');
+    });
+    $('#modalCrimsonPoison').on('hidden.bs.modal', function () {
+        $('.pie_progress_CrimsonPoison').asPieProgress('reset');
+    });
+
+    $('#modalBullguard').on('shown.bs.modal', function () {
+        $('.pie_progress_Bullguard').asPieProgress('start');
+    });
+    $('#modalBullguard').on('hidden.bs.modal', function () {
+        $('.pie_progress_Bullguard').asPieProgress('reset');
+    });
+});
